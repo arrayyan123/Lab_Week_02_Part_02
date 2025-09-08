7e9a19b1aaf4eeb8cb055b49c5f8a1f321164cb9
arrayyan aprilyanto <146199861+arrayyan123@users.noreply.github.com>
2025-09-08 08:38:53 +0700

    Commit No. 3 – Fix application crash when user input wrong color code [Intent]”

--- app/src/main/java/com/arrayyan/lab_week_02_part_02/ResultActivity.kt
+++ app/src/main/java/com/arrayyan/lab_week_02_part_02/ResultActivity.kt
@@ -1,18 +1,19 @@
 package com.arrayyan.lab_week_02_part_02
 
+import android.app.Activity
 import android.graphics.Color
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.constraintlayout.widget.ConstraintLayout
 
 
 class ResultActivity : AppCompatActivity() {
     companion object {
         private const val COLOR_KEY = "COLOR_KEY"
+        private const val ERROR_KEY = "ERROR_KEY"
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,7 +22,17 @@ class ResultActivity : AppCompatActivity() {
             val colorCode = intent.getStringExtra(COLOR_KEY)
             val backgroundScreen =
                 findViewById<ConstraintLayout>(R.id.background_screen)
-            backgroundScreen.setBackgroundColor(Color.parseColor("#$colorCode"))
+            try {
+                backgroundScreen.setBackgroundColor(Color.parseColor("#$colorCode"))
+            }
+            catch (ex: IllegalArgumentException){
+                Intent().let{
+                        errorIntent ->
+                    errorIntent.putExtra(ERROR_KEY, true)
+                    setResult(Activity.RESULT_OK, errorIntent)
+                    finish()
+                }
+            }
             val resultMessage =
                 findViewById<TextView>(R.id.color_code_result_message)
             resultMessage.text = getString(R.string.color_code_result_message,
